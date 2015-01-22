95758d0d769f26ea06776505f289f282e76c8217
FrankFan <fanyong@gmail.com>
2015-01-23 00:00:04 +0800

    add js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
 console.log('\'Allo \'Allo!');
+
+// js 作用域
+
+(function a(){
+	var foo = 1;
+
+	if(true){
+		foo = 2;
+		for(var i = 3; i <= 5; i++){
+			foo = i;
+			console.log(foo);
+		}
+	}
+})();
+
+
+// 在函数中用var声明变量
+var foo = function(){
+	var boo = function(){
+		bar = 2;
+	}();
+}();
+
+console.log(bar);
