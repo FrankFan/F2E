7debd927a3af210a952b6c75414de4b8998f26f9
FrankFan <fanyong@gmail.com>
2015-01-26 23:49:20 +0800

    js clone

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
 console.log('\'Allo \'Allo!');
+
+
+ 
+function clone(myObj){ 
+	if(typeof(myObj) != 'object') 
+		return myObj; 
+	if(myObj == null) return myObj;
+
+	var myNewObj = {}; 
+	for(var i in myObj) 
+		myNewObj[i] = clone(myObj[i]); 
+	return myNewObj; 
+}
+
+var a = { name: 'a', test: function(){ console.log('hi'); }, b:{ str: 'hehe'}}
+var b = clone(a);
+console.log(b);
