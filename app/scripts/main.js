89e3734749e1edbd11f4ea57af6bf08871a8c493
FrankFan <fanyong@gmail.com>
2015-02-22 10:54:57 +0800

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,3 +18,21 @@ var b = clone(a);
 console.log(b);
 
 console.dir(typeof('ff'));
+
+
+
+Javascript 原型
+
+原型是Javascript面向对象特性中的重要的概念，也是大家不太熟悉的概念。
+
+function Person() {
+
+}
+
+Person.prototype.name = 'Frank';
+Person.prototype.showName = function() {
+	console.log(this.name);
+};
+
+var person = new Person();
+person.showName();
